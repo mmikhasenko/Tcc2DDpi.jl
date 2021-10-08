053f599022d124e563e12d3b9c02e611f6e965e5
Misha Mikhasenko <mikhail.mikhasenko@gmail.com>
2021-10-08 21:42:14 +0200

    update to complex plane

--- scripts/plotcomplexplane_3d_plotlyjs.jl
+++ scripts/plotcomplexplane_3d_plotlyjs.jl
@@ -1,10 +1,9 @@
 using X2DDpi
 
-using Plotly
-using PlotlyJS, LaTeXStrings
+using Plotly, LaTeXStrings
 
 # get data
-fin = readjson(joinpath("results","nominal","complexplane_model_without_interference.json"))
+fin = readjson(joinpath("results","nominal","logA_2d_grid.json"))
 xi = Vector{Float64}(fin["x"])
 yi = Vector{Float64}(fin["y"])
 zi = Matrix{Float64}(hcat(fin["z"]...))
@@ -15,22 +14,23 @@ end
 
 zexts = extrema(zi)
 dz = (zexts[2]-zexts[1])
-zlims = (zexts[1]-0.2*dz, zexts[2]-0.1*dz)
+zlims = (zexts[1]-0.2*dz, zexts[2]-0.05*dz)
 
 zcut = zexts[1]+0.05*dz
-yc, xc = let ij = findmin(zi)[2]
-    yi[ij[1]], xi[ij[2]] 
-end
+yc, xc = -81.3e-3/2, 0.0
+# let ij = findmin(zi)[2]
+#     yi[ij[1]], xi[ij[2]] 
+# end
 
 
 layout = Layout(
-    title=L"T_{cc}^+\to D^{0}D^{0}\pi^+\,\,\mathrm{amplitude\,\,in\,\,the\,\,complex\,\,plane.\,\,Schematic\,\,view}",
+    title=L"T_{cc}^+\to D^{0}D^{0}\pi^+\,\,\mathrm{amplitude\,\,in\,\,the\,\,complex\,\,plane.}",
     # autosize=true,
     automargin=true,
     # paper_bgcolor="LightSteelBlue",
-    scene_camera_eye=attr(x=0.8, y=-2.0, z=0.6),
+    scene_camera_eye=attr(x=0.8, y=-2.0, z=0.4),
     width=650, height=550,
-    scene_camera_center = attr(x=-0.1, y=0, z=-0.1),
+    scene_camera_center = attr(x=-0.1, y=0, z=-0.25),
     scene = attr(xaxis_title="Re √s [MeV]",
                  yaxis_title="Im √s [MeV]",
                  zaxis_title="log |A|²",
@@ -51,7 +51,7 @@ layout = Layout(
                     text=L"D^{*+}D^0\,\,\mathrm{branch\,\,point}",
                     textangle=0,
                     ax=0,
-                    ay=-75,
+                    ay=-55,
                     font=attr(
                         color="black",
                         size=12
@@ -84,11 +84,14 @@ f = plot(surface(
     z=zi',
     showscale=false,
     colorscale="Viridis",
+    zmin=zexts[1]-0.15dz,
+    zmax=zlims[2],
+    # reversescale=true,
     contours=attr(
         y=attr(show=true, start=0.0, size=0.1, color="red", project_y=true),
         y_end=0.01,
-        z=attr(show=true, start=zlims[1], size=0.5, usecolormap=true, project_z=true),
-        z_end=zexts[2]-0.15dz,
+        z=attr(show=true, start=zlims[1], size=0.3, usecolormap=true, project_z=true),
+        z_end=zlims[2]-0.2dz,
     ),
 ), layout)
 # savefig(f, joinpath("plots","nominal","complexplane_model_without_interference.html"))
@@ -96,9 +99,9 @@ f = plot(surface(
 # savefig(f, joinpath("plots","nominal","complexplane_model_without_interference.pdf"))
 
 
-Plotly.set_credentials_file(Dict("username"=>"misha_mikhasenko","api_key"=>"dpeK5rPdg5E3z93DOMgN"))
+# Plotly.set_credentials_file(Dict("username"=>"misha_mikhasenko","api_key"=>"dpeK5rPdg5E3z93DOMgN"))
 
 Plotly.signin("misha_mikhasenko","dpeK5rPdg5E3z93DOMgN")
 remote_plot = post(f)
-Plotly.set_credentials_file(Dict("username"=>"misha_mikhasenko","api_key"=>"dpeK5rPdg5E3z93DOMgN"))
+# Plotly.set_credentials_file(Dict("username"=>"misha_mikhasenko","api_key"=>"dpeK5rPdg5E3z93DOMgN"))
 
