34797ce5137ecd43c96775e51ad926346626e737
Misha Mikhasenko <mikhail.mikhasenko@gmail.com>
2021-11-14 18:44:06 +0100

    update the model

--- scripts/model.jl
+++ scripts/model.jl
@@ -9,9 +9,9 @@ settings = transformdictrecursively!(readjson("settings.json"), ifstringgivemeas
 @unpack cutoff, estep = settings["phspmatching"]
 #
 channels = [
-    πDD((m1=mπ⁺,m2=mD⁰,m3=mD⁰), (m=mDˣ⁺,Γ=ΓDˣ⁺), (m=mDˣ⁺,Γ=ΓDˣ⁺)),
-    πDD((m1=mπ⁰,m2=mD⁺,m3=mD⁰), (m=mDˣ⁺,Γ=ΓDˣ⁺), (m=mDˣ⁰,Γ=ΓDˣ⁰)),
-    γDD((m1=mγ, m2=mD⁺,m3=mD⁰), (m=mDˣ⁺,Γ=ΓDˣ⁺), (m=mDˣ⁰,Γ=ΓDˣ⁰))]
+    πDD((m1=mπ⁺,m2=mD⁰,m3=mD⁰), BW(m=mDˣ⁺,Γ=ΓDˣ⁺), BW(m=mDˣ⁺,Γ=ΓDˣ⁺)),
+    πDD((m1=mπ⁰,m2=mD⁺,m3=mD⁰), BW(m=mDˣ⁺,Γ=ΓDˣ⁺), BW(m=mDˣ⁰,Γ=ΓDˣ⁰)),
+    γDD((m1=mγ, m2=mD⁺,m3=mD⁰), BW(m=mDˣ⁺,Γ=ΓDˣ⁺), BW(m=mDˣ⁰,Γ=ΓDˣ⁰))]
 #
 ichannels = interpolated.(channels, cutoff; estep=estep) # cutoff
 
