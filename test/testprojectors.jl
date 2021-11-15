9cc741464ff7b76bd178593b1d90584360ea3b22
Misha Mikhasenko <mikhail.mikhasenko@gmail.com>
2021-11-15 08:58:44 +0100

    automatic norm of bw_e

--- test/testprojectors.jl
+++ test/testprojectors.jl
@@ -14,9 +14,7 @@ bw_e(s,e0,Γ) = J_I(s,
     BW_norm(m=e2m(e0), Γ=Γ*1e-3, n=sqrt(e2m(e0)*(Γ*1e-3) / π)))
 # 
 const srange = (e2m.(δm0_val .+ (-1, 1) .* 2Γ0)) .^ 2
-const bwn = 1/quadgk(s->abs2(bw_e(s,δm0_val,Γ0)), e2m(δm0_val-2Γ0)^2, e2m(δm0_val+2Γ0)^2)[1]
-
-(e2m(δm0_val-2Γ0)^2, e2m(δm0_val+2Γ0)^2)
+const bwn = quadgk(s->abs2(bw_e(s,δm0_val,Γ0)), e2m(δm0_val-2Γ0)^2, e2m(δm0_val+2Γ0)^2)[1]
 
 
 import X2DDpi: projectto3
@@ -34,7 +32,7 @@ let d = π⁺D⁰D⁰, m = e2m(δm0_val)
     plot(e3->projectto3(d, m^2, e3^2),
         2.004, 2.011, lab="")
 # 
-    plot!(e3->projectto3(d, e3^2)*1.19,
+    plot!(e3->projectto3(d, e3^2) / bwn,
         2.004, 2.011, lab="")
 end
 
@@ -45,7 +43,6 @@ projectto1(d, σ1) = projectto1(d, σ1,
     s->bw_e(s,δm0_val,Γ0),
     srange...)
 #
-2mD⁰, e2m(δm0_val)-mπ⁺
 # 
 @time projectto1(π⁺D⁰D⁰, e2m(δm0_val)^2, 3.730^2)
 @time projectto1(π⁺D⁰D⁰, 3.730^2)
@@ -55,6 +52,6 @@ let d = π⁺D⁰D⁰, m = e2m(δm0_val)
     plot(e1->projectto1(d, m^2, e1^2),
         3.729, 3.737, lab="")
 # 
-    plot!(e1->projectto1(d, e1^2)*1.19,
+    plot!(e1->projectto1(d, e1^2) / bwn,
         3.729, 3.737, lab="")
 end
