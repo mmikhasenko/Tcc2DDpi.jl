bbb8829312430a38529fddf141be101c3b4c7a76
Misha Mikhasenko <mikhail.mikhasenko@gmail.com>
2021-11-15 08:27:24 +0100

    test projections works

--- test/testprojectors.jl
+++ test/testprojectors.jl
@@ -1,23 +1,30 @@
 using X2DDpi
+using Plots
+# 
+theme(:wong2, minorticks=true, grid=false, frame=:box,
+    guidefontvalign=:top, guidefonthalign=:right,
+    foreground_color_legend = nothing,
+    legendfontsize=9, legend =:topright,
+    xlim=(:auto,:auto), ylim=(:auto,:auto), lab="")
 
-
-const δm0_val = -359.2e-3
-
-function bw_e(s,e0,Γ)
-    m = e2m(e0)
-    return sqrt(m*(Γ*1e-3) / π) / (m^2-s-1im*m*(Γ*1e-3))
-end
+const δm0_val = -359.2e-3 # MeV
+const Γ0 = 48e-3 # MeV
 # 
 bw_e(s,e0,Γ) = J_I(s,
     BW_norm(m=e2m(e0), Γ=Γ*1e-3, n=sqrt(e2m(e0)*(Γ*1e-3) / π)))
+# 
+const srange = (e2m.(δm0_val .+ (-1, 1) .* 2Γ0)) .^ 2
+const bwn = 1/quadgk(s->abs2(bw_e(s,δm0_val,Γ0)), e2m(δm0_val-2Γ0)^2, e2m(δm0_val+2Γ0)^2)[1]
+
+(e2m(δm0_val-2Γ0)^2, e2m(δm0_val+2Γ0)^2)
 
 
 import X2DDpi: projectto3
 projectto3(d, σ3) = projectto3(d, σ3,
     s->bw_e(s,δm0_val,Γ0),
-    e2m(δm0_val-2Γ0)^2, e2m(δm0_val+2Γ0)^2)
+    srange...)
 
-π⁺D⁰D⁰ = πDD((m1=mπ⁺,m2=mD⁰,m3=mD⁰), (m=mDˣ⁺,Γ=ΓDˣ⁺), (m=mDˣ⁺,Γ=ΓDˣ⁺))
+π⁺D⁰D⁰ = πDD((m1=mπ⁺,m2=mD⁰,m3=mD⁰), BW(m=mDˣ⁺,Γ=ΓDˣ⁺), BW(m=mDˣ⁺,Γ=ΓDˣ⁺))
 
 @time projectto3(π⁺D⁰D⁰, e2m(δm0_val)^2, 2.007^2)
 @time projectto3(π⁺D⁰D⁰, 2.007^2)
@@ -29,4 +36,25 @@ let d = π⁺D⁰D⁰, m = e2m(δm0_val)
 # 
     plot!(e3->projectto3(d, e3^2)*1.19,
         2.004, 2.011, lab="")
+end
+
+
+
+import X2DDpi: projectto1
+projectto1(d, σ1) = projectto1(d, σ1,
+    s->bw_e(s,δm0_val,Γ0),
+    srange...)
+#
+2mD⁰, e2m(δm0_val)-mπ⁺
+# 
+@time projectto1(π⁺D⁰D⁰, e2m(δm0_val)^2, 3.730^2)
+@time projectto1(π⁺D⁰D⁰, 3.730^2)
+
+# should look similar
+let d = π⁺D⁰D⁰, m = e2m(δm0_val)
+    plot(e1->projectto1(d, m^2, e1^2),
+        3.729, 3.737, lab="")
+# 
+    plot!(e1->projectto1(d, e1^2)*1.19,
+        3.729, 3.737, lab="")
 end
