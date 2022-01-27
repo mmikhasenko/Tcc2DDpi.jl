c980225c76c6742c6bbbbd50a0e0c1c3a7cf991c
Mikhail Mikhasenko <mikhail.mikhasenko@cern.ch>
2022-01-27 10:33:55 +0100

    terrible mistake of first second sheet J

--- src/isobar.jl
+++ src/isobar.jl
@@ -20,8 +20,8 @@ abstract type AbstractLinesShape end
     m::Float64
     Γ::Float64
 end
-Jᴵ( σ, pars::BW) = σ == pars.m^2-1im*pars.m*pars.Γ ? 0.0 : 1.0 / (pars.m^2 - σ - 1im*pars.m*pars.Γ)
-Jᴵᴵ(σ, pars::BW) = σ == pars.m^2+1im*pars.m*pars.Γ ? 0.0 : 1.0 / (pars.m^2 - σ + 1im*pars.m*pars.Γ)
+Jᴵ( σ, pars::BW) = σ == pars.m^2-1im*pars.m*pars.Γ ? 0.0 : 1.0 / (pars.m^2 - σ + 1im*pars.m*pars.Γ)
+Jᴵᴵ(σ, pars::BW) = σ == pars.m^2+1im*pars.m*pars.Γ ? 0.0 : 1.0 / (pars.m^2 - σ - 1im*pars.m*pars.Γ)
 
 pole_position(R::BW) = R.m^2-1im*R.m*R.Γ
 
@@ -31,16 +31,16 @@ pole_position(R::BW) = R.m^2-1im*R.m*R.Γ
     ma::Float64
     mb::Float64
 end
-Jᴵ( σ, pars::BW_Swave) = 1 / (pars.m^2 - σ - 1im*pars.g^2*sqrt(λ(σ,pars.ma^2,pars.mb^2))/σ)
-Jᴵᴵ(σ, pars::BW_Swave) = 1 / (pars.m^2 - σ + 1im*pars.g^2*sqrt(λ(σ,pars.ma^2,pars.mb^2))/σ)
+Jᴵ( σ, pars::BW_Swave) = 1 / (pars.m^2 - σ + 1im*pars.g^2*sqrt(λ(σ,pars.ma^2,pars.mb^2))/σ)
+Jᴵᴵ(σ, pars::BW_Swave) = 1 / (pars.m^2 - σ - 1im*pars.g^2*sqrt(λ(σ,pars.ma^2,pars.mb^2))/σ)
 
 @with_kw struct BW_norm <: AbstractLinesShape
     m::Float64
     Γ::Float64
 	n::Float64
 end
-Jᴵ( σ, pars::BW_norm) = pars.n / (pars.m^2 - σ - 1im*pars.m*pars.Γ)
-Jᴵᴵ(σ, pars::BW_norm) = pars.n / (pars.m^2 - σ + 1im*pars.m*pars.Γ)
+Jᴵ( σ, pars::BW_norm) = pars.n / (pars.m^2 - σ + 1im*pars.m*pars.Γ)
+Jᴵᴵ(σ, pars::BW_norm) = pars.n / (pars.m^2 - σ - 1im*pars.m*pars.Γ)
 
 
 function obj2nt(amp::AbstractLinesShape)
