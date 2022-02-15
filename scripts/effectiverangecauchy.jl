566c4edc7af5070b904d169684655ef26863961a
Mikhail Mikhasenko <mikhail.mikhasenko@cern.ch>
2022-02-15 11:43:07 +0100

    separated plotting of compositeness

--- scripts/effectiverangecauchy.jl
+++ scripts/effectiverangecauchy.jl
@@ -193,6 +193,17 @@ print(select(df[[1,2],:], [:modelnames, :a_fm, :r_fm, :N]))
 
 
 
+Xc(r::Real,a⁻¹::Real) = 1/sqrt(1+2r*a⁻¹/X2DDpi.fm_times_mev)
+Xc(r::Complex,a⁻¹::Complex) = Xc(real(r),real(a⁻¹))
+# 
+Z_polosa_90 = map(r->Xc(r, df.a⁻¹[1]*1e3), (-11.9,0) .+ df.r_fm[1])
+Z_polosa_95 = map(r->Xc(r, df.a⁻¹[1]*1e3), (-16.9,0) .+ df.r_fm[1])
+# 
+Z_hanhart_90 = map(r->Xc(r, df.a⁻¹[1]*1e3), (-11.9,0) .+ df.r_fm[2])
+Z_hanhart_95 = map(r->Xc(r, df.a⁻¹[1]*1e3), (-16.9,0) .+ df.r_fm[2])
+
+
+
 writejson(joinpath("results","nominal","effectiverangecauchy.json"),
         Dict(
             :allmodelcomputation => [
@@ -219,42 +230,6 @@ writejson(joinpath("results","nominal","effectiverangecauchy.json"),
             )
     ))
 #
-
-
-
-
-Xc(r::Real,a⁻¹::Real) = 1/sqrt(1+2r*a⁻¹/X2DDpi.fm_times_mev)
-Xc(r::Complex,a⁻¹::Complex) = Xc(real(r),real(a⁻¹))
-# 
-Z_polosa_90 = map(r->Xc(r, df.a⁻¹[1]*1e3), (-11.9,0) .+ df.r_fm[1])
-Z_polosa_95 = map(r->Xc(r, df.a⁻¹[1]*1e3), (-16.9,0) .+ df.r_fm[1])
-# 
-Z_hanhart_90 = map(r->Xc(r, df.a⁻¹[1]*1e3), (-11.9,0) .+ df.r_fm[2])
-Z_hanhart_95 = map(r->Xc(r, df.a⁻¹[1]*1e3), (-16.9,0) .+ df.r_fm[2])
-
-
-
-let
-    plot(size=(500,120), yaxis=false, yticks=false, frame=:origin, xlim=(0,1), ylim=(-0.6,0.6),
-        ann=(1,0.1,text(L"X",12,:bottom,:right)))
-    plot!([Z_polosa_90...], 0.5 .* [1, 1], fill=0, c=2, α=0.2, lab="")
-    plot!([Z_polosa_95...], 0.5 .* [0.8, 0.8], fill=0, c=2, α=0.2, lab="")
-    annotate!([
-        (Z_polosa_90[1], 0.5, text(string(round(Z_polosa_90[1],digits=2)),:bottom, 8)),
-        (Z_polosa_90[2], 0.5, text(string(round(Z_polosa_90[2],digits=2)),:bottom, 8)),
-        (Z_polosa_95[1], 0.4, text(string(round(Z_polosa_95[1],digits=2)),:bottom, 8)),
-        ])
-    plot!([Z_hanhart_90...], 0.5 .* [-1, -1], fill=0, c=3, α=0.2, lab="")
-    plot!([Z_hanhart_95...], 0.5 .* [-0.8, -0.8], fill=0, c=3, α=0.2, lab="")
-    annotate!([
-        (Z_hanhart_90[1], -0.5, text(string(round(Z_hanhart_90[1],digits=2)),:top, 8)),
-        (Z_hanhart_90[2], -0.5, text(string(round(Z_hanhart_90[2],digits=2)),:top, 8)),
-        (Z_hanhart_95[1], -0.4, text(string(round(Z_hanhart_95[1],digits=2)),:top, 8)),
-        ])
-end
-savefig(joinpath("plots", "compositeness.pdf"))
-
-
 
 
 
---
+++ scripts/plotcompositeness.jl
@@ -0,0 +1,46 @@
+using Pkg
+cd(joinpath(@__DIR__,".."))
+Pkg.activate(".")
+# 
+using X2DDpi
+using Parameters
+using Measurements
+using Interpolations
+using Statistics
+
+using Plots
+using LaTeXStrings
+
+theme(:wong2, frame=:box, grid=false, minorticks=true, 
+    guidefontvalign=:top, guidefonthalign=:right)
+
+
+effectiverangeresults = readjson(joinpath("results","nominal","effectiverangecauchy.json"))
+
+
+@unpack Z_polosa_90, Z_polosa_95,
+    Z_hanhart_90, Z_hanhart_95 = 
+        effectiverangeresults["compositeness"]
+# 
+import Plots.PlotMeasures:mm
+let
+    plot(size=(500,120), yaxis=false, yticks=false, frame=:origin, xlim=(0,1), ylim=(-0.6,0.6),
+        ann=(1,0.1,text(L"X",12,:bottom,:right)))
+    plot!([Z_polosa_90...], 0.5 .* [1, 1], fill=0, c=2, α=0.2, lab="")
+    plot!([Z_polosa_95...], 0.5 .* [0.8, 0.8], fill=0, c=2, α=0.2, lab="")
+    annotate!([
+        (Z_polosa_90[1], 0.5, text(string(round(Z_polosa_90[1],digits=2)),:bottom, 8)),
+        (Z_polosa_90[2], 0.5, text(string(round(Z_polosa_90[2],digits=2)),:bottom, 8)),
+        (Z_polosa_95[1], 0.4, text(string(round(Z_polosa_95[1],digits=2)),:bottom, 8)),
+        ])
+    plot!([Z_hanhart_90...], 0.5 .* [-1, -1], fill=0, c=3, α=0.2, lab="")
+    plot!([Z_hanhart_95...], 0.5 .* [-0.8, -0.8], fill=0, c=3, α=0.2, lab="")
+    annotate!([
+        (Z_hanhart_90[1], -0.5, text(string(round(Z_hanhart_90[1],digits=2)),:top, 8)),
+        (Z_hanhart_90[2], -0.5, text(string(round(Z_hanhart_90[2],digits=2)),:top, 8)),
+        (Z_hanhart_95[1], -0.4, text(string(round(Z_hanhart_95[1],digits=2)),:top, 8)),
+        ])
+    plot!([0,0], [-0.06,0.06], c=:black, lab="", ann=(0.01,-0.06,text("0.0",:top,8)))
+end
+savefig(joinpath("plots", "compositeness.pdf"))
+
