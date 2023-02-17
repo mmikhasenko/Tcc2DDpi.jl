3cfe364a2be12e8f137ed607b8511a2b1b14e0ae
Mikhail Mikhasenko <mikhail.mikhasenko@cern.ch>
2023-02-17 16:41:46 +0100

    renamed models

--- README.md
+++ README.md
@@ -44,44 +44,44 @@ returns a complex value, for which `Im(dispersive) = ρ_thr`.
 
 1. **Default analysis**
 
-   - `S-000-model.jl` build the default model, cache the tables
-   - `S-001-computeasymptotics.jl` save ρInf
-   - `S-002-effectiverange.jl` value of the scattering parameters using the matching method
-   - `S-003-plotinverseamplitude.jl` matching plot for the inverse amplitude
-   - `S-004-plotwidthdependance.jl` profile for different values Γ
-   - `S-005-DpDz_spectrum.jl` projection to D+D0
-   - `S-006-Dpi_spectrum.jl` projection to Dpi
-   - `S-007-polesearch.jl` default pole position
-   - `S-008-polescang.jl` the width saturation curve
-   - `S-009-plotcomplexplane.jl` plot the complex plane for the paper
-   - `S-010-interference_matrix.jl` contribution of the decay-chain interference
-   - `S-011-pimDpDp.jl` why π⁻D⁺D⁺ is negligible
-   - `S-012-loopdiagrams.jl` plot loop diagrams for the supplemental material
-   - `S-013-Dxcomplexmass.jl` Tcc parameters in the model with complex D* mass
+   - [`S-000-model.jl`](scripts/S-000-model.jl) build the default model, cache the tables
+   - [`S-001-computeasymptotics.jl`](scripts/S-001-computeasymptotics.jl) save ρInf
+   - [`S-002-effectiverange.jl`](scripts/S-002-effectiverange.jl) value of the scattering parameters using the matching method
+   - [`S-003-plotinverseamplitude.jl`](scripts/S-003-plotinverseamplitude.jl) matching plot for the inverse amplitude
+   - [`S-004-plotwidthdependance.jl`](scripts/S-004-plotwidthdependance.jl) profile for different values Γ
+   - [`S-005-DpDz_spectrum.jl`](scripts/S-005-DpDz_spectrum.jl) projection to D+D0
+   - [`S-006-Dpi_spectrum.jl`](scripts/S-006-Dpi_spectrum.jl) projection to Dpi
+   - [`S-007-polesearch.jl`](scripts/S-007-polesearch.jl) default pole position
+   - [`S-008-polescang.jl`](scripts/S-008-polescang.jl) the width saturation curve
+   - [`S-009-plotcomplexplane.jl`](scripts/S-009-plotcomplexplane.jl) plot the complex plane for the paper
+   - [`S-010-interference_matrix.jl`](scripts/S-010-interference_matrix.jl) contribution of the decay-chain interference
+   - [`S-011-pimDpDp.jl`](scripts/S-011-pimDpDp.jl) why π⁻D⁺D⁺ is negligible
+   - [`S-012-loopdiagrams.jl`](scripts/S-012-loopdiagrams.jl) plot loop diagrams for the supplemental material
+   - [`S-013-Dxcomplexmass.jl`](scripts/S-013-Dxcomplexmass.jl) Tcc parameters in the model with complex D* mass
 
 2. **Angular dependence**
 
-   - `S-100-dalitz_plots.jl` dalitz plot for different quantum numbers
-   - `S-101-angular_distribution.jl` anglular distribution (sympy) along the D* band
-   - `S-102-profile_peakandtail.jl` fraction of the tail in different models
-   - `S-103-DpionL.jl` Sensiticity of the Dpi spectrum on quantum numbers
+   - [`S-100-dalitz_plots.jl`](scripts/S-100-dalitz_plots.jl) dalitz plot for different quantum numbers
+   - [`S-101-angular_distribution.jl`](scripts/S-101-angular_distribution.jl) anglular distribution (sympy) along the D* band
+   - [`S-102-profile_peakandtail.jl`](scripts/S-102-profile_peakandtail.jl) fraction of the tail in different models
+   - [`S-103-DpionL.jl`](scripts/S-103-DpionL.jl) Sensiticity of the Dpi spectrum on quantum numbers
 
 3. **Isospin dependence**
 
-   - `S-200-pole_vs_g1g2.jl`
+   - [`S-200-pole_vs_g1g2.jl`](scripts/S-200-pole_vs_g1g2.jl)
 
 4. **Complex plane** for [EP news](https://ep-news.web.cern.ch/content/lhcb-discovers-double-charm-tetraquark)
 
-   - `S-300-complexplaneongrid.jl` Compute the values on the complex grid using the default model
-   - `S-301-DxD_model_without_interference.jl` Compute the values on the complex grid using a simplified model
-   - `S-302-plotcomplexplane_3d_plotlyjs.jl` Visualize the values
+   - [`S-300-complexplaneongrid.jl`](scripts/S-300-complexplaneongrid.jl) Compute the values on the complex grid using the default model
+   - [`S-301-DxD_model_without_interference.jl`](scripts/S-301-DxD_model_without_interference.jl) Compute the values on the complex grid using a simplified model
+   - [`S-302-plotcomplexplane_3d_plotlyjs.jl`](scripts/S-302-plotcomplexplane_3d_plotlyjs.jl) Visualize the values
 
 5. **Scattering parameters using the cauchy integrals**
 
-   - `S-400-effectiverangecauchy.jl`
-   - `S-401-plotcompositeness.jl`
+   - [`S-400-effectiverangecauchy.jl`](scripts/S-400-effectiverangecauchy.jl)
+   - [`S-401-plotcompositeness.jl`](scripts/S-401-plotcompositeness.jl)
 
 6. **Additional plots for talks**
 
-   - `S-900-howwidthismade.jl` contributions to the width
-   - `S-901-xxx.jl` DDpi momentum spectrum. Femtoscopy
+   - [`S-900-howwidthismade.jl`](scripts/S-900-howwidthismade.jl) contributions to the width
+   - [`S-901-xxx.jl`](scripts/S-901-xxx.jl) DDpi momentum spectrum. Femtoscopy
--- scripts/S-400-effectiverangecauchy.jl
+++ scripts/S-400-effectiverangecauchy.jl
@@ -1,5 +1,5 @@
 using Pkg
-cd(joinpath(@__DIR__,".."))
+cd(joinpath(@__DIR__, ".."))
 Pkg.activate(".")
 # 
 using X2DDpi
@@ -10,11 +10,11 @@ using Statistics
 using LaTeXStrings
 
 using Plots
-theme(:wong2, size=(500,350), minorticks=true, grid=false, frame=:box,
+theme(:wong2, size=(500, 350), minorticks=true, grid=false, frame=:box,
     guidefontvalign=:top, guidefonthalign=:right,
-    foreground_color_legend = nothing,
-    legendfontsize=9, legend =:topright,
-    xlim=(:auto,:auto), ylim=(:auto,:auto))
+    foreground_color_legend=nothing,
+    legendfontsize=9, legend=:topright,
+    xlim=(:auto, :auto), ylim=(:auto, :auto))
 
 
 #        _|              _|                
@@ -45,10 +45,10 @@ In includes three three-body cuts, π⁺D⁰D⁰ + π⁰D⁰D⁺ + γD⁰D⁺ an
  - Dˣ⁺ and Dˣ⁰ in π⁰D⁰D⁺
  - Dˣ⁺ and Dˣ⁰ in γD⁰D⁺
 """
-@time const A₀_full = let 
-    ch1 = πDD((m1=mπ⁺,m2=mD⁰,m3=mD⁰), BW(m=mDˣ⁺,Γ=ΓDˣ⁺), BW(m=mDˣ⁺,Γ=ΓDˣ⁺))
-    ch2 = πDD((m1=mπ⁰,m2=mD⁺,m3=mD⁰), BW(m=mDˣ⁺,Γ=ΓDˣ⁺), BW(m=mDˣ⁰,Γ=ΓDˣ⁰))
-    ch3 = γDD((m1=mγ, m2=mD⁺,m3=mD⁰), BW(m=mDˣ⁺,Γ=ΓDˣ⁺), BW(m=mDˣ⁰,Γ=ΓDˣ⁰))
+@time const model0 = let
+    ch1 = πDD((m1=mπ⁺, m2=mD⁰, m3=mD⁰), BW(m=mDˣ⁺, Γ=ΓDˣ⁺), BW(m=mDˣ⁺, Γ=ΓDˣ⁺))
+    ch2 = πDD((m1=mπ⁰, m2=mD⁺, m3=mD⁰), BW(m=mDˣ⁺, Γ=ΓDˣ⁺), BW(m=mDˣ⁰, Γ=ΓDˣ⁰))
+    ch3 = γDD((m1=mγ, m2=mD⁺, m3=mD⁰), BW(m=mDˣ⁺, Γ=ΓDˣ⁺), BW(m=mDˣ⁰, Γ=ΓDˣ⁰))
     # 
     iπDD2 = interpolated(
         ChannelWithIntegrationMethod(ch1, HookSqrtDalitzMapping{2}()),
@@ -71,7 +71,7 @@ In includes three three-body cuts, π⁺D⁰D⁰ + π⁰D⁰D⁺ + γD⁰D⁺ an
         ChannelWithIntegrationMethod(ch3, HookSqrtDalitzMapping{3}()),
         cutoff; estep=estep)
     # 
-    Amplitude((iπDD2,iπDD3,iπDD2′,iπDD3′,iγDD2,iγDD3))
+    Amplitude((iπDD2, iπDD3, iπDD2′, iπDD3′, iγDD2, iγDD3))
 end
 
 
@@ -83,10 +83,10 @@ However, it includes three three-body cuts, π⁺D⁰D⁰ + π⁰D⁰D⁺ + γD�
  - one Dˣ⁺ in π⁰D⁰D⁺
  - one Dˣ⁺ in γD⁰D⁺
 """
-@time const A₀_full_DˣD = let 
+@time const model1 = let
     # ch1 = πDD((m1=mπ⁺,m2=mD⁰,m3=mD⁰), BW(m=mDˣ⁺,Γ=ΓDˣ⁺), BW(m=mDˣ⁺,Γ=ΓDˣ⁺))
-    ch2 = πDD((m1=mπ⁰,m2=mD⁺,m3=mD⁰), BW(m=mDˣ⁺,Γ=ΓDˣ⁺), ZeroBW(m=mDˣ⁰,Γ=ΓDˣ⁰))
-    ch3 = γDD((m1=mγ, m2=mD⁺,m3=mD⁰), BW(m=mDˣ⁺,Γ=ΓDˣ⁺), ZeroBW(m=mDˣ⁰,Γ=ΓDˣ⁰))
+    ch2 = πDD((m1=mπ⁰, m2=mD⁺, m3=mD⁰), BW(m=mDˣ⁺, Γ=ΓDˣ⁺), ZeroBW(m=mDˣ⁰, Γ=ΓDˣ⁰))
+    ch3 = γDD((m1=mγ, m2=mD⁺, m3=mD⁰), BW(m=mDˣ⁺, Γ=ΓDˣ⁺), ZeroBW(m=mDˣ⁰, Γ=ΓDˣ⁰))
     # 
     # iπDD2 = interpolated(
     #     ChannelWithIntegrationMethod(ch1, HookSqrtDalitzMapping{2}()),
@@ -94,7 +94,7 @@ However, it includes three three-body cuts, π⁺D⁰D⁰ + π⁰D⁰D⁺ + γD�
     # iπDD3 = interpolated(
     #     ChannelWithIntegrationMethod(ch1, HookSqrtDalitzMapping{3}()),
     #     cutoff; estep=estep)
-    iπDD2, iπDD3 = A₀_full.ik[1], A₀_full.ik[2]
+    iπDD2, iπDD3 = model0.ik[1], model0.ik[2]
     # 
     iπDD3′ = interpolated(
         ChannelWithIntegrationMethod(ch2, HookSqrtDalitzMapping{3}()),
@@ -104,29 +104,29 @@ However, it includes three three-body cuts, π⁺D⁰D⁰ + π⁰D⁰D⁺ + γD�
         ChannelWithIntegrationMethod(ch3, HookSqrtDalitzMapping{3}()),
         cutoff; estep=estep)
     # 
-    Amplitude((iπDD2,iπDD3,iπDD3′,iγDD3))
+    Amplitude((iπDD2, iπDD3, iπDD3′, iγDD3))
 end
 
 # πDD: Dˣ⁺ + Dˣ⁺
 """
 The model with the π⁺D⁰D⁰ system with two Dˣ⁺ resonances.
 """
-const A₀_π⁺D⁰D⁰ = let
+const model2 = let
     # ch1 = πDD((m1=mπ⁺,m2=mD⁰,m3=mD⁰), BW(m=mDˣ⁺,Γ=ΓDˣ⁺), BW(m=mDˣ⁺,Γ=ΓDˣ⁺))
-    iπDD2, iπDD3 = A₀_full.ik[1], A₀_full.ik[2]
+    iπDD2, iπDD3 = model0.ik[1], model0.ik[2]
     # 
     # set3 = ChannelWithIntegrationMethod(ch, HookSqrtDalitzMapping{3}())
     # ich3 = interpolated(set3, cutoff; estep=estep)
     # 
-    Amplitude((iπDD2,iπDD3))
+    Amplitude((iπDD2, iπDD3))
 end
 
 # πDD: Dˣ⁺
 """
 The model with the π⁺D⁰D⁰ system with a single, unsymmetrized Dˣ⁺ resonance.
 """
-const A₀_DˣD = let
-    ch = πDD((m1=mπ⁺,m2=mD⁰,m3=mD⁰), BW(m=mDˣ⁺,Γ=ΓDˣ⁺), ZeroBW(m=mDˣ⁺,Γ=ΓDˣ⁺))
+const model3 = let
+    ch = πDD((m1=mπ⁺, m2=mD⁰, m3=mD⁰), BW(m=mDˣ⁺, Γ=ΓDˣ⁺), ZeroBW(m=mDˣ⁺, Γ=ΓDˣ⁺))
     # 
     set3 = ChannelWithIntegrationMethod(ch, HookSqrtDalitzMapping{3}())
     ich3 = interpolated(set3, cutoff; estep=estep)
@@ -135,13 +135,13 @@ const A₀_DˣD = let
 end
 
 # πDD: Dˣ⁺
-const A₀′_DˣD = Amplitude(
+const model4 = Amplitude(
     interpolated(
-        DˣD((m1=mπ⁺,m2=mD⁰,m3=mD⁰), BW(m=mDˣ⁺, Γ=ΓDˣ⁺)),
+        DˣD((m1=mπ⁺, m2=mD⁰, m3=mD⁰), BW(m=mDˣ⁺, Γ=ΓDˣ⁺)),
         cutoff; estep=estep))
 # 
 # 
-modelnames = [:A₀_full, :A₀_full_DˣD, :A₀_π⁺D⁰D⁰, :A₀_DˣD, :A₀′_DˣD]
+modelnames = [:model0, :model1, :model2, :model3, :model4]
 const Nm = length(modelnames)
 
 
@@ -151,31 +151,31 @@ using DataFrames
 df = DataFrame(; modelnames)
 df.model = eval.(df.modelnames)
 
-let 
+let
     @time effrangepars = # 12s
         effectiverangeexpansion(
-            Δe->denominator_II(A₀_full, Eᵦˣ⁺+Δe, δm0_val),
-            Δe->k3b(Eᵦˣ⁺+Δe),
-            ComplexBranchPointExpansion(CircularSum(abs(imag(Eᵦˣ⁺))/20, 50)))
+            Δe -> denominator_II(model0, Eᵦˣ⁺ + Δe, δm0_val),
+            Δe -> k3b(Eᵦˣ⁺ + Δe),
+            ComplexBranchPointExpansion(CircularSum(abs(imag(Eᵦˣ⁺)) / 20, 50)))
     # 
     (; tophysicsunits(effrangepars)..., effrangepars...)
 end
 
 df.a_fm = ones(Nm)
-df.r_fm = ones(Nm).*1im
-df.a⁻¹ = ones(Nm).*1im
-df.r = ones(Nm).*1im
-df.N = ones(Nm).*1im
+df.r_fm = ones(Nm) .* 1im
+df.a⁻¹ = ones(Nm) .* 1im
+df.r = ones(Nm) .* 1im
+df.N = ones(Nm) .* 1im
 
 
-let Nappr = 150, Rexp = abs(imag(Eᵦˣ⁺))/20
+let Nappr = 150, Rexp = abs(imag(Eᵦˣ⁺)) / 20
     # 
     for i in 1:Nm
         𝒜 = df.model[i]
         @time effrangepars = # 12s
             effectiverangeexpansion(
-                Δe->denominator_II(𝒜, Eᵦˣ⁺+Δe, δm0_val),
-                Δe->k3b(Eᵦˣ⁺+Δe),
+                Δe -> denominator_II(𝒜, Eᵦˣ⁺ + Δe, δm0_val),
+                Δe -> k3b(Eᵦˣ⁺ + Δe),
                 ComplexBranchPointExpansion(CircularSum(Rexp, Nappr)))
         # 
         efrpars = (; tophysicsunits(effrangepars)..., effrangepars...)
@@ -191,17 +191,17 @@ end
 
 # uncertainties
 df.a_fm⁺ᵟ = ones(Nm)
-df.r_fm⁺ᵟ = ones(Nm).*1im
-df.a⁻¹⁺ᵟ = ones(Nm).*1im
-df.r⁺ᵟ = ones(Nm).*1im
-df.N⁺ᵟ = ones(Nm).*1im
+df.r_fm⁺ᵟ = ones(Nm) .* 1im
+df.a⁻¹⁺ᵟ = ones(Nm) .* 1im
+df.r⁺ᵟ = ones(Nm) .* 1im
+df.N⁺ᵟ = ones(Nm) .* 1im
 df.a_fm⁻ᵟ = ones(Nm)
-df.r_fm⁻ᵟ = ones(Nm).*1im
-df.a⁻¹⁻ᵟ = ones(Nm).*1im
-df.r⁻ᵟ = ones(Nm).*1im
-df.N⁻ᵟ = ones(Nm).*1im
+df.r_fm⁻ᵟ = ones(Nm) .* 1im
+df.a⁻¹⁻ᵟ = ones(Nm) .* 1im
+df.r⁻ᵟ = ones(Nm) .* 1im
+df.N⁻ᵟ = ones(Nm) .* 1im
 
-let Nappr = 150, Rexp = abs(imag(Eᵦˣ⁺))/20
+let Nappr = 150, Rexp = abs(imag(Eᵦˣ⁺)) / 20
     # 
     for i in 1:Nm
         𝒜 = df.model[i]
@@ -209,8 +209,8 @@ let Nappr = 150, Rexp = abs(imag(Eᵦˣ⁺))/20
         # dm0+δdm0
         @time effrangepars⁺ᵟ = # 12s
             effectiverangeexpansion(
-                Δe->denominator_II(𝒜, Eᵦˣ⁺+Δe, δm0_val+δm0.err),
-                Δe->k3b(Eᵦˣ⁺+Δe),
+                Δe -> denominator_II(𝒜, Eᵦˣ⁺ + Δe, δm0_val + δm0.err),
+                Δe -> k3b(Eᵦˣ⁺ + Δe),
                 ComplexBranchPointExpansion(CircularSum(Rexp, Nappr)))
         # 
         efrpars⁺ᵟ = (; tophysicsunits(effrangepars⁺ᵟ)..., effrangepars⁺ᵟ...)
@@ -224,8 +224,8 @@ let Nappr = 150, Rexp = abs(imag(Eᵦˣ⁺))/20
         # dm0-δdm0
         @time effrangepars⁻ᵟ = # 12s
             effectiverangeexpansion(
-                Δe->denominator_II(𝒜, Eᵦˣ⁺+Δe, δm0_val-δm0.err),
-                Δe->k3b(Eᵦˣ⁺+Δe),
+                Δe -> denominator_II(𝒜, Eᵦˣ⁺ + Δe, δm0_val - δm0.err),
+                Δe -> k3b(Eᵦˣ⁺ + Δe),
                 ComplexBranchPointExpansion(CircularSum(Rexp, Nappr)))
         # 
         efrpars⁻ᵟ = (; tophysicsunits(effrangepars⁻ᵟ)..., effrangepars⁻ᵟ...)
@@ -240,41 +240,41 @@ end
 
 
 
-ps((a,b,c)) = "$(a)^{+$(b)}_{-$(c)}"
+ps((a, b, c)) = "$(a)^{+$(b)}_{-$(c)}"
 
 # test
-print(select(df[[4,5],:], [:modelnames, :a_fm, :r_fm, :N]))
+print(select(df[[4, 5], :], [:modelnames, :a_fm, :r_fm, :N]))
 # modelnames  a_fm      r_fm                  N
 #─────────────────────────────────────────────────────────────────────
-# A₀_DˣD      -7.55077  0.159869-0.0238077im  0.00347571-3.76763e-5im
-# A₀′_DˣD     -7.55237  0.165517-0.0216824im   0.0034758-3.76788e-5im
+# model3      -7.55077  0.159869-0.0238077im  0.00347571-3.76763e-5im
+# model4     -7.55237  0.165517-0.0216824im   0.0034758-3.76788e-5im
 
 # interference term
-print(select(df[[3,4],:], [:modelnames, :a_fm, :r_fm, :N]))
+print(select(df[[3, 4], :], [:modelnames, :a_fm, :r_fm, :N]))
 # modelnames  a_fm      r_fm                   N
 #───────────────────────────────────────────────────────────────────────
-# A₀_π⁺D⁰D⁰   -7.31763  -0.779907+0.260606im   0.00702464+0.000589854im
-# A₀_DˣD      -7.55077   0.159869-0.0238077im  0.00347571-3.76763e-5im
+# model2   -7.31763  -0.779907+0.260606im   0.00702464+0.000589854im
+# model3      -7.55077   0.159869-0.0238077im  0.00347571-3.76763e-5im
 
 # higher threshold
-print(select(df[[1,2],:], [:modelnames, :a_fm, :r_fm, :N]))
+print(select(df[[1, 2], :], [:modelnames, :a_fm, :r_fm, :N]))
 # modelnames  a_fm      r_fm                   N
 #──────────────────────────────────────────────────────────────────────
-# A₀_full      -5.95934   -4.36196+0.4608im    0.0103207+0.000671301im
-# A₀_full_DˣD  -7.32118  -0.626768+0.159036im   0.010316+0.000554445im
+# model0      -5.95934   -4.36196+0.4608im    0.0103207+0.000671301im
+# model1  -7.32118  -0.626768+0.159036im   0.010316+0.000554445im
 
 
 # Discussion of the results:
 # model          a_fm            r_fm
 #─────────────────────────────────────────────
-# A₀_DˣD      -7.55077   0.159869-0.0238077im  
-# A₀_π⁺D⁰D⁰   -7.31763  -0.779907+0.260606im   
-# A₀_full_DˣD  -7.32118  -0.626768+0.159036im  
-# A₀_full      -5.95934   -4.36196+0.4608im    
+# model3      -7.55077   0.159869-0.0238077im  
+# model2   -7.31763  -0.779907+0.260606im   
+# model1  -7.32118  -0.626768+0.159036im  
+# model0      -5.95934   -4.36196+0.4608im    
 # 
-# - The effective range is positive +0.15 fm for the model with one Dˣ⁺ resonance (A₀_DˣD).
+# - The effective range is positive +0.15 fm for the model with one Dˣ⁺ resonance (model3).
 # The positive slope comes from the two-body loop integral. 
-# - Once the OPE is included (A₀_π⁺D⁰D⁰), | Dˣ⁺ + Dˣ⁺ |², the effective range turns negative, -0.78 fm
+# - Once the OPE is included (model2), | Dˣ⁺ + Dˣ⁺ |², the effective range turns negative, -0.78 fm
 # - The next step is to add two other three-body thresholds, D⁰D⁺π⁰ and D⁰D⁺γ, where the Dˣ⁺ can decay, the effective range changes unsignificantly, -0.63 fm
 # - The full model is obtained by adding the Dˣ⁰D⁺ threshold, the value of the effective range is shifted to -4.36 fm
 
@@ -282,62 +282,62 @@ print(select(df[[1,2],:], [:modelnames, :a_fm, :r_fm, :N]))
 
 function printuncertainty(nt, s, processing=identity)
     v = nt[s]
-    v⁺ᵟ = nt[Symbol(s,"⁺ᵟ")]
-    v⁻ᵟ = nt[Symbol(s,"⁻ᵟ")]
-    Δv⁺ = v⁺ᵟ-v
-    Δv⁻ = v-v⁻ᵟ
+    v⁺ᵟ = nt[Symbol(s, "⁺ᵟ")]
+    v⁻ᵟ = nt[Symbol(s, "⁻ᵟ")]
+    Δv⁺ = v⁺ᵟ - v
+    Δv⁻ = v - v⁻ᵟ
     # 
-    abc = [v,Δv⁺,Δv⁻]
+    abc = [v, Δv⁺, Δv⁻]
     return ps(processing(x) for x in abc)
 end
 
 
 
-Xc(r::Real,a⁻¹::Real) = 1/sqrt(1+2r*a⁻¹/X2DDpi.fm_times_mev)
-Xc(r::Complex,a⁻¹::Complex) = Xc(real(r),real(a⁻¹))
+Xc(r::Real, a⁻¹::Real) = 1 / sqrt(1 + 2r * a⁻¹ / X2DDpi.fm_times_mev)
+Xc(r::Complex, a⁻¹::Complex) = Xc(real(r), real(a⁻¹))
 # 
-Z_polosa_90 = map(r->Xc(r, df.a⁻¹[1]*1e3), (-11.9,0) .+ df.r_fm[1])
-Z_polosa_95 = map(r->Xc(r, df.a⁻¹[1]*1e3), (-16.9,0) .+ df.r_fm[1])
+Z_polosa_90 = map(r -> Xc(r, df.a⁻¹[1] * 1e3), (-11.9, 0) .+ df.r_fm[1])
+Z_polosa_95 = map(r -> Xc(r, df.a⁻¹[1] * 1e3), (-16.9, 0) .+ df.r_fm[1])
 # 
-Z_hanhart_90 = map(r->Xc(r, df.a⁻¹[1]*1e3), (-11.9,0) .+ df.r_fm[2])
-Z_hanhart_95 = map(r->Xc(r, df.a⁻¹[1]*1e3), (-16.9,0) .+ df.r_fm[2])
+Z_hanhart_90 = map(r -> Xc(r, df.a⁻¹[1] * 1e3), (-11.9, 0) .+ df.r_fm[2])
+Z_hanhart_95 = map(r -> Xc(r, df.a⁻¹[1] * 1e3), (-16.9, 0) .+ df.r_fm[2])
 
 
 
 
-writejson(joinpath("results","nominal","effectiverangecauchy.json"),
-        Dict(
-            :allmodelcomputation => [
-                Symbol(df.modelnames[i]) => Dict{Symbol,Any}(
-                    :a_fm => df.a_fm[i],
-                    :r_fm => df.r_fm[i],
-                    :inv_a => df.a⁻¹[i],
-                    :r => df.r[i],
-                    :N => df.N[i]
-                ) for i in 1:size(df,1)],
-# 
-            :investigationsummary => Dict(
-                :inv_scatt_length_MeV => round(df.a⁻¹[1]*1e3, digits=2),
-                :effective_range_fm => round(df.r_fm[1], digits=2),
-                :effective_range_disp_fm => round(df.r_fm[2], digits=2),
-                :effective_range_high_fm => round(df.r_fm[1]-df.r_fm[2], digits=2),
-            ),
-# 
-            :withuncertainties => Dict(
-                :inv_scatt_length_MeV_Re => printuncertainty(df[1,:], :a⁻¹, x->round(real(1e3*x),digits=2)),
-                :inv_scatt_length_MeV_Im => printuncertainty(df[1,:], :a⁻¹, x->round(imag(1e3*x),digits=2)),
-                :effective_range_fm_Re => printuncertainty(df[1,:], :r_fm, x->round(real(x),digits=2)),
-                :effective_range_fm_Im => printuncertainty(df[1,:], :r_fm, x->round(imag(x),digits=2)),
-                :effective_range_disp_fm_Re => printuncertainty(df[2,:], :r_fm, x->round(real(x),digits=2)),
-                :effective_range_high_fm_Im => printuncertainty(df[2,:], :r_fm, x->round(real(x),digits=2))
-            ),
-# 
-            :compositeness => Dict(
-                :Z_hanhart_90 => Z_hanhart_90,
-                :Z_hanhart_95 => Z_hanhart_95,
-                :Z_polosa_90 => Z_polosa_90,
-                :Z_polosa_95 => Z_polosa_95
-            )
+writejson(joinpath("results", "nominal", "effectiverangecauchy.json"),
+    Dict(
+        :allmodelcomputation => [
+            Symbol(df.modelnames[i]) => Dict{Symbol,Any}(
+                :a_fm => df.a_fm[i],
+                :r_fm => df.r_fm[i],
+                :inv_a => df.a⁻¹[i],
+                :r => df.r[i],
+                :N => df.N[i]
+            ) for i in 1:size(df, 1)],
+        # 
+        :investigationsummary => Dict(
+            :inv_scatt_length_MeV => round(df.a⁻¹[1] * 1e3, digits=2),
+            :effective_range_fm => round(df.r_fm[1], digits=2),
+            :effective_range_disp_fm => round(df.r_fm[2], digits=2),
+            :effective_range_high_fm => round(df.r_fm[1] - df.r_fm[2], digits=2),
+        ),
+        # 
+        :withuncertainties => Dict(
+            :inv_scatt_length_MeV_Re => printuncertainty(df[1, :], :a⁻¹, x -> round(real(1e3 * x), digits=2)),
+            :inv_scatt_length_MeV_Im => printuncertainty(df[1, :], :a⁻¹, x -> round(imag(1e3 * x), digits=2)),
+            :effective_range_fm_Re => printuncertainty(df[1, :], :r_fm, x -> round(real(x), digits=2)),
+            :effective_range_fm_Im => printuncertainty(df[1, :], :r_fm, x -> round(imag(x), digits=2)),
+            :effective_range_disp_fm_Re => printuncertainty(df[2, :], :r_fm, x -> round(real(x), digits=2)),
+            :effective_range_high_fm_Im => printuncertainty(df[2, :], :r_fm, x -> round(real(x), digits=2))
+        ),
+        # 
+        :compositeness => Dict(
+            :Z_hanhart_90 => Z_hanhart_90,
+            :Z_hanhart_95 => Z_hanhart_95,
+            :Z_polosa_90 => Z_polosa_90,
+            :Z_polosa_95 => Z_polosa_95
+        )
     ))
 #
 
@@ -354,30 +354,30 @@ struct EFRMatching
 end
 EFRMatching(𝒜, nt) = EFRMatching(𝒜, ERA(nt.a⁻¹, nt.r, nt.N))
 
-function X2DDpi.denominator_II(era::ERA,e)
+function X2DDpi.denominator_II(era::ERA, e)
     @unpack a⁻¹, r, N = era
-    return N*(a⁻¹ + r/2*k3b(e)^2-1im*k3b(e))
+    return N * (a⁻¹ + r / 2 * k3b(e)^2 - 1im * k3b(e))
 end
 
 using RecipesBase
 @recipe function f(m::EFRMatching)
     Δev = range(-0.01, 0.0251, length=30)
     @series begin
-        calv = [denominator_II(m.era, Eᵦˣ⁺+Δe) for Δe in Δev]
+        calv = [denominator_II(m.era, Eᵦˣ⁺ + Δe) for Δe in Δev]
         label := "effective range"
-        (Δev, real(calv)) 
+        (Δev, real(calv))
     end
-    calv = [denominator_II(m.𝒜, Eᵦˣ⁺+Δe, δm0_val) for Δe in Δev]
+    calv = [denominator_II(m.𝒜, Eᵦˣ⁺ + Δe, δm0_val) for Δe in Δev]
     (Δev, real(calv))
 end
 
 let
-    plot(size=(200*5,150), layout=grid(1,5), yaxis=false, yticks=false)
-    plot!(sp=1,EFRMatching(df.model[1], df[1,:]), lab=string(df.modelnames[1]))
-    plot!(sp=2,EFRMatching(df.model[2], df[2,:]), lab=string(df.modelnames[2]))
-    plot!(sp=3,EFRMatching(df.model[3], df[3,:]), lab=string(df.modelnames[3]))
-    plot!(sp=4,EFRMatching(df.model[4], df[4,:]), lab=string(df.modelnames[4]))
-    plot!(sp=5,EFRMatching(df.model[5], df[5,:]), lab=string(df.modelnames[5]))
+    plot(size=(200 * 5, 150), layout=grid(1, 5), yaxis=false, yticks=false)
+    plot!(sp=1, EFRMatching(df.model[1], df[1, :]), lab=string(df.modelnames[1]))
+    plot!(sp=2, EFRMatching(df.model[2], df[2, :]), lab=string(df.modelnames[2]))
+    plot!(sp=3, EFRMatching(df.model[3], df[3, :]), lab=string(df.modelnames[3]))
+    plot!(sp=4, EFRMatching(df.model[4], df[4, :]), lab=string(df.modelnames[4]))
+    plot!(sp=5, EFRMatching(df.model[5], df[5, :]), lab=string(df.modelnames[5]))
 end
 savefig(joinpath("plots", "testmatchefr.pdf"))
 
@@ -388,18 +388,18 @@ savefig(joinpath("plots", "testmatchefr.pdf"))
 N′ = 1 / (w_matching * rho_inf)
 let i = 1
     plot(xlab=L"\delta' m\,\,(\mathrm{MeV})", ylab=L"\mathrm{Re}\,\mathcal{A}^{-1}(black),\,\,\mathrm{Im}\,\mathcal{A}^{-1}(red)")
-    plot!(Δe->N′*real(denominator_II(df.model[i], Δe, δm0_val)), -1, 3, lab="LHCb Model", lc=:black)
-    plot!(Δe->N′*imag(denominator_II(df.model[i], Δe, δm0_val)), -1, 3, lab="", lc=:red)
+    plot!(Δe -> N′ * real(denominator_II(df.model[i], Δe, δm0_val)), -1, 3, lab="LHCb Model", lc=:black)
+    plot!(Δe -> N′ * imag(denominator_II(df.model[i], Δe, δm0_val)), -1, 3, lab="", lc=:red)
     # 
-    @unpack a⁻¹, r, N = df[i,:]
-    era = ERA( a⁻¹, r, N)
-    plot!(Δe->N′*real(denominator_II(era, Δe)), -1, 3, lab="Eff.-range exp.", ls=:dash, lc=:black)
-    plot!(Δe->N′*imag(denominator_II(era, Δe)), -1, 3, lab="", ls=:dash, lc=:red)
+    @unpack a⁻¹, r, N = df[i, :]
+    era = ERA(a⁻¹, r, N)
+    plot!(Δe -> N′ * real(denominator_II(era, Δe)), -1, 3, lab="Eff.-range exp.", ls=:dash, lc=:black)
+    plot!(Δe -> N′ * imag(denominator_II(era, Δe)), -1, 3, lab="", ls=:dash, lc=:red)
     # plot!(Δe->imag(2*denominator_II(df.model[4], Eᵦˣ⁺+Δe, δm0_val)),-0.01, 0.1, lab=string(df.modelnames[4]))
     vline!([0], lab="", c=:green, lw=1)
     # scatter!([0.0], [N′*real(denominator_II(era, 0))], ms=5, mc=:black, lab="exp. point")
     # scatter!([0.0], [N′*imag(denominator_II(era, 0))], ms=5, mc=:red, lab="")
 end
-savefig(joinpath("plots","nominal", "effectiverangecauchy.pdf"))
+savefig(joinpath("plots", "nominal", "effectiverangecauchy.pdf"))
 
 
