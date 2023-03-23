7fff99c79ab2cfda78e02f1a5b9aea1775cff352
Mikhail Mikhasenko <mikhail.mikhasenko@cern.ch>
2023-03-23 11:31:22 +0100

    alignment

--- src/fiteffectiverange.jl
+++ src/fiteffectiverange.jl
@@ -18,17 +18,17 @@ function effectiverangeexpansion(f, k, method::EffectiveRangeFit)
     function mismatch(x)
         a⁻¹, r, N = x[1:3] .+ 1im .* x[4:6]
         yv′ = ere.(k.(ev); a⁻¹, r, N)
-        χ² = sum(abs2, yv-yv′)
+        χ² = sum(abs2, yv - yv′)
         return χ² / n
     end
 
     p = NamedTuple{(:a⁻¹, :r, :N)}(start)
     x0 = [real.([p...])..., imag.([p...])...]
     optim_result = Optim.optimize(mismatch, x0, Optim.BFGS())
-    
+
     xmin = optim_result.minimizer
     a⁻¹, r, N = xmin[1:3] .+ 1im .* xmin[4:6]
 
-    return (; a⁻¹, r, N, minimum=optim_result.minimum, χ0 = mismatch(x0))
+    return (; a⁻¹, r, N, minimum=optim_result.minimum, χ0=mismatch(x0))
 end
 
