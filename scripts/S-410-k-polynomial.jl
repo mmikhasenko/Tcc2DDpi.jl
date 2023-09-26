98a6e1577d19f7f977313cb0de3996a9d111a78b
Misha Mikhasenko <mikhail.mikhasenko@cern.ch>
2023-09-26 18:14:22 +0200

    prototype epsilon extrapolation

--- scripts/S-410-k-polynomial.jl
+++ scripts/S-410-k-polynomial.jl
@@ -36,6 +36,91 @@ theme(:wong2, size=(500, 350), minorticks=true, grid=false, frame=:box,
     legendfontsize=9, legend=:topright, lab="",
     xlim=(:auto, :auto), ylim=(:auto, :auto))
 
+# 
+#      _|_|
+#    _|      _|    _|  _|_|_|      _|_|_|
+#  _|_|_|_|  _|    _|  _|    _|  _|
+#    _|      _|    _|  _|    _|  _|
+#    _|        _|_|_|  _|    _|    _|_|_|
+
+
+const RegressionResult = NamedTuple{(:α, :β, :SE_α, :SE_β)}
+import Base: *
+*(nt::RegressionResult, f::Number) = RegressionResult(collect(nt) .* f)
+
+function Base.show(io::IO, nt::RegressionResult)
+    print(io,
+        "Interpolation to zero:",
+        "\n\tintercept: ", round(nt.β, digits=2),
+        "\n\tinterpolation error: ", round(nt.SE_β, digits=2),
+        "\n\tslope: ", round(nt.α, digits=2),"\n")
+end
+
+
+"""
+    linear_regression(xv, yv)
+
+Perform linear regression on the given data points `xv` and `yv`.
+
+# Arguments
+- `xv::Vector`: A vector of x-values.
+- `yv::Complex`: A vector of corresponding y-values.
+
+# Returns
+- `α::Complex`: The slope of the linear regression.
+- `β::Complex`: The intercept of the linear regression.
+- `SE_α_over_α::Complex`: The standard error of the slope as δα_re/|α| + j*δα_im/|α|.
+- `SE_β_over_β::Complex`: The standard error of the intercept as δβ_re/|α| + j*δβ_im/|α|.
+
+# Example
+```julia
+x = [1.0, 2.0, 3.0, 4.0, 5.0]
+y = [2.1, 4.0, 6.1, 8.0, 10.2]
+result = linear_regression(x, y)
+
+# Note
+The function computes the intercept and slope using the formulae derived from the method of least squares.
+    It also computes the standard errors for the intercept and slope to provide a measure of the uncertainty associated with these estimates. 
+"""
+function linear_regression(xv, yv)
+    Ex = mean(xv)
+    Ey = mean(yv)
+    Exy = mean(xv .* yv)
+    Ex² = mean(xv .^ 2)
+    Dx = Ex² - Ex^2
+
+    # α is a slope, β is the intercept 
+    α, β = [Exy - Ex * Ey, Ey * Ex² - Ex * Exy] ./ Dx
+
+    # Compute residuals
+    residuals = yv .- (β .+ α .* xv)
+
+    # Compute residual sum of squares
+    RSS_real = sum(real.(residuals) .^ 2)
+    RSS_imag = sum(imag.(residuals) .^ 2)
+
+    # Compute standard error of the regression
+    n = length(xv)
+    dof = n - 2
+    SER_real = sqrt(RSS_real / dof)
+    SER_imag = sqrt(RSS_imag / dof)
+
+    # Compute standard error of the slope
+    SE_β_real = SER_real / sqrt(sum((xv .- Ex) .^ 2))
+    SE_β_imag = SER_imag / sqrt(sum((xv .- Ex) .^ 2))
+
+    # Compute standard error of the intercept
+    SE_α_real = SE_β_real * sqrt(1/n + Ex^2 / sum((xv .- Ex) .^ 2))
+    SE_α_imag = SE_β_imag * sqrt(1/n + Ex^2 / sum((xv .- Ex) .^ 2))
+
+    # SE complex
+    SE_α = SE_α_real + 1im*SE_α_imag
+    SE_β = SE_β_real + 1im*SE_β_imag
+
+    return RegressionResult((; α, β, SE_α, SE_β))
+end
+
+
 
 #        _|              _|                
 #    _|_|_|    _|_|_|  _|_|_|_|    _|_|_|  
@@ -48,6 +133,7 @@ settings = transformdictrecursively!(readjson("settings.json"), ifstringgivemeas
 @unpack δm0 = settings["fitresults"]
 const δm0_val = δm0.val
 @unpack cutoff, estep = settings["phspmatching"]
+
 
 
 #                            _|            
@@ -83,10 +169,10 @@ the circular Cauchy integrals are related to the expansion parameters of $R(s)$.
 We find that the expansion of the function is not great in the complex plane in the range ~0.1 MeV.
 """
 
-function effrange_denominator_II_k3b(model; method)
+function effrange_denominator_II_k3b(model, δm0; method)
     effrangepars =
         effectiverangeexpansion(
-            Δe -> denominator_II(model, Eᵦˣ⁺ + Δe, δm0_val),
+            Δe -> denominator_II(model, Eᵦˣ⁺ + Δe, δm0),
             Δe -> k3b(Eᵦˣ⁺ + Δe),
             method)
     # 
@@ -103,7 +189,7 @@ Before further studies, let check how strong the dependce on r, that we are figh
 """
 
 df_radius = map([0.0001, 0.001, 0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1, 2]) do ϵf
-    efe = effrange_denominator_II_k3b(model2;
+    efe = effrange_denominator_II_k3b(model2, δm0_val;
         method=ComplexBranchPointExpansion(CircularSum(ϵf * ϵ0, 50)))
     # 
     (; ϵf, efe...)
@@ -127,7 +213,7 @@ We are going to expand
 """
 
 cauchysum = ComplexBranchPointExpansion(CircularSum(ϵ0 / 20, 50))
-efe2 = effrange_denominator_II_k3b(model2; method=cauchysum)
+efe2 = effrange_denominator_II_k3b(model2, δm0_val; method=cauchysum)
 
 C0(f, ϵ, N=50) = circleintegral(f, 0.0, CircularSum(ϵ, N))
 
@@ -139,30 +225,21 @@ C0_D = C0(cauchysum.cim.r) do Δe
 end
 @assert efe2.N == C0_D / C0_k / (-1im)
 
-function linear_regression(xv,yv)
-    Ex = mean(xv)
-    Ey = mean(yv)
-    Exy = mean(xv.*yv)
-    Ex² = mean(xv.^2)
-    Dx = Ex² - Ex^2
-    # 
-    α,β = [Exy - Ex*Ey, Ey*Ex² - Ex*Exy] ./ Dx
-    return (; α,β)
-end
+
 
 let
     ϵfv = df_radius.ϵf[4:end-3]
     xv = sqrt.(ϵfv)
     yv = df_radius.r[4:end-3]
-    linear_regression(xv,yv)
+    regression = linear_regression(xv,yv)
+    @show regression
+    @unpack α, β = regression
+    # 
+    plot(xv, yv .|> real)
+    plot!(x-> real(β + α * x), range(xv[[1,end]]..., 30))
 end
 
-md"""
-The extraction algorithm will be:
-1. build model,
-2. effrange_denominator_II_k3b
-3. linear_regression
-"""
+
 
 let
     model = model2
@@ -277,3 +354,73 @@ The deviation is of an order
 
 is seen. It is much smaller the deviation effect in Re Im pars.
 """
+
+
+md"""
+# Summary
+
+The dependence on the integral radius is natural and understood.
+The correction is linear in √circle_r. The functions below do the intepolation
+
+The extraction algorithm will be:
+1. build model,
+2. scan in ϵf with `effective_range_scan`,
+3. extrapolate to zero with `effective_range_extrapolation`
+"""
+
+
+function effective_range_scan(model, settings)
+    @unpack ϵ0 = settings
+    @unpack ϵf_grid = settings
+    @unpack circular_sum_n = settings
+    @unpack model_δm0 = settings
+    # 
+    method(r) = ComplexBranchPointExpansion(CircularSum(r, circular_sum_n))
+
+    scan = map(ϵf_grid) do ϵf
+        circle_r = ϵf * ϵ0
+        effrangepars =
+            effectiverangeexpansion(
+                Δe -> denominator_II(model, Eᵦˣ⁺ + Δe, model_δm0),
+                Δe -> k3b(Eᵦˣ⁺ + Δe),
+                method(circle_r))
+        efe = (; tophysicsunits(effrangepars)..., effrangepars..., ϵf, circle_r)
+        efe
+    end
+
+    return scan
+end
+
+function linear_regression_in_sqrt_ϵf(scan, quantity)
+    ϵfv = getproperty.(scan, :ϵf)
+    sqrt_ϵfv = sqrt.(ϵfv)
+    # 
+    calv = getproperty.(scan, quantity)
+    regression = linear_regression(sqrt_ϵfv, calv)
+    # 
+    return regression
+end
+
+function effective_range_extrapolation(scan)
+    r_regression = linear_regression_in_sqrt_ϵf(scan, :r)
+    inva_regression = linear_regression_in_sqrt_ϵf(scan, :a⁻¹)
+    # 
+    (; r_regression, inva_regression)
+end
+
+
+# apply to the model as a test
+
+effective_range_intepolation_settings = ( 
+    ϵ0 = abs(imag(Eᵦˣ⁺)),
+    ϵf_grid = [0.02, 0.05, 0.1, 0.2],
+    circular_sum_n = 50,
+    model_δm0 = δm0_val)
+
+ere_scan = effective_range_scan(model2,
+    effective_range_intepolation_settings)
+# 
+ssc = effective_range_extrapolation(ere_scan)
+
+@show ssc.r_regression;
+@show ssc.inva_regression * 1000;
